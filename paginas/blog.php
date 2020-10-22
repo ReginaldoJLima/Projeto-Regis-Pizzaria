c4671ef1446b5959228b936bface259c10645e4b
dianababrosa <diana2011barbosa@gmail.com>
2020-10-22 11:45:46 -0300

    Atualiazações do dia 22/10/2020

--- paginas/blog.php
+++ paginas/blog.php
@@ -2,3 +2,69 @@
 <div class="container-fluid text-center marginsBanner">
     <img src="public/img/blog.jpg" alt="" class="img-fluid w-100">
 </div>
+
+<!-- início do blog -->
+
+<div class="card" style="width: 84rem;">
+    <img src="public/img/pizzaLiteratura.jpg" class="card-img-top" alt="">
+    <div class="card-body">
+        <p class="card-text">Pizza é Literatura!
+            Capa do livro Pizzarias na Itália
+            <hr>
+            Pizzarias em Buenos Aires, nossa primeira resenha. Juntar várias coisas que amamos é um dos principais objetivos que temos na vida, não? Por isso, fico feliz em postar aqui que Pizza é Literatura!
+
+            Na primeira postagem desta nova (e, se o Deus da Pizza quiser, permanente) fase do Blog da Pizza, falamos sobre um Guia de Pizzarias de Buenos Aires. E, a partir de hoje, criamos uma série nova no blog, Pizza é literatura, que trará, a cada duas semanas (mais ou menos), resenhas de livros que tratam do mundo da nossa redonda.
+
+            Como disse no início, quando a gente consegue colocar em um mesmo lugar várias coisas que amamos é como se chegássemos à Terra Prometida. Esse blog, de certa forma, é um pedaço (com trocadilho) desta terra, pois consegui juntar Tecnologia/convergência das mídias + Pizza + Livro/literatura. E isso tem a ver com a história da minha vida…</p>
+    </div>
+</div>
+<!-- segunda parte do blog -->
+
+<!-- 1ª caixinha do blog -->
+<div class="card-group">
+    <div class="card bg-warning">
+        <img src="public/img/nossapizzadacasa.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">O livro de pizza da casa</h5>
+            <p class="card-text">O primeiro livro publicado no Brasil sobre pizzas foi uma tradução do Il libro della Pizza ou O Livro da Pizza, de Vincenzo Buonassisi, editado pelo Círculo do Livro, na década de 80 do século passado. Um das lembranças mais fortes que eu tenho, de meados da década de 1970 e 1980, é a dos meus pais chamando os quatro filhos para “lerem” o catálogo do Círculo do Livro e escolherem um livro. A expectativa que isso trazia, com certeza, muitos dos jovens de hoje têm com os clubs de assinatura como TAG, Turista Literário ou Leiturinha, só para citar alguns (há alguns anos, tenho orientado TCCs em Produção Editorial com este modelo de negócio, o que mostra que o mercado tem encontrado caminhos para a crise na distribuição de livros no Brasil, mas isso é outra história…).</p>
+            <p class="card-text"><small class="text-muted">
+PUBLICADO EM17 DE JANEIRO DE 2019
+O Livro da Pizza
+Il Libro della Pizza
+O primeiro livro publicado no Brasil sobre pizzas foi uma tradução do Il libro della Pizza ou O Livro da Pizza, de Vincenzo Buonassisi, editado pelo Círculo do Livro, na década de 80 do século passado. Além de ter sido um marco na história entre pizza e literatura, esse título traz muitas recordações…
+
+Logo do Círculo do Livro
+Um das lembranças mais fortes que eu tenho, de meados da década de 1970 e 1980, é a dos meus pais chamando os quatro filhos para “lerem” o catálogo do Círculo do Livro e escolherem um livro. A expectativa que isso trazia, com certeza, muitos dos jovens de hoje têm com os clubs de assinatura como TAG, Turista Literário ou Leiturinha, só para citar alguns (há alguns anos, tenho orientado TCCs em Produção Editorial com este modelo de negócio, o que mostra que o mercado tem encontrado caminhos para a crise na distribuição de livros no Brasil, mas isso é outra história…).
+Época de pegar a estrada (literalmente ou não) e curtir outros ares, carregando as baterias para a outra metade do ano.
+</small></p>
+        </div>
+
+    </div>
+
+    <!-- 2ª caixinha do blog -->
+
+    <div class="card">
+        <img src="public/img/parceria.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Parcerias</h5>
+            <p class="card-text">Vivemos, no Brasil, um momento de grande turbulência. O país está dividido e o futuro é incerto. Aprendi com meu pai que, normalmente, os problemas da sociedade surgem quando esquecemos de pensar no coletivo em prol de algo individual. Precisamos muito, nos dias de hoje, de mais amor, ainda mais se ele vier na forma de uma pizza… 😎
+
+Todo e qualquer ato de altruísmo ou de compaixão nos dias de hoje deve ser divulgado para todos os cantos, de forma a mostrar que a humanidade ainda tem solução. 
+
+Nada melhor que um exemplo ligado à nossa amada redonda… Então, aí vai a história de Julie e Rich Morgan, que nos dá esperança de um amanhã mais feliz.</p>
+            <p class="card-text"><small class="text-muted">Agradecimentos aos nossos grandes parceiros por estar juntamente conosco construindo essa grande história!!!</small></p>
+        </div>
+    </div>
+
+    <!-- 3ª caixinha do blog -->
+    <div class="card bg-warning">
+        <img src="public/img/amigosdacasa.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Amigos da casa</h5>
+            <p class="card-text">Na primeira postagem desta nova (e, se o Deus quiser, permanente) fase do Blog do Reg Pizza, falamos sobre um Guia de Pizzarias de Buenos Aires. E, a partir de hoje, criamos uma série nova no blog, Pizza é literatura, que trará, a cada duas semanas (mais ou menos), resenhas de livros que tratam do mundo da nossa redonda.
+
+                Como disse no início, quando a gente consegue colocar em um mesmo lugar várias coisas que amamos é como se chegássemos à Terra Prometida. Esse blog, de certa forma, é um pedaço (com trocadilho) desta terra, pois consegui juntar Tecnologia/convergência das mídias + Pizza + Livro/literatura. E isso tem a ver com a história da minha vida.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+    </div>
+</div>
